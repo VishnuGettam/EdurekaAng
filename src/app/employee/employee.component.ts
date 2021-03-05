e299323fe5002efbe327317efe5a4607d09f1899
VishnuGettam <vishnugettam@gmail.com>
2021-03-05 14:37:27 +0530

    Assignment 2 Changes
    Added the Code for Components- Assignment 2

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+  styleUrls: ['./employee.component.css'],
 })
 export class EmployeeComponent implements OnInit {
+  employee: Employee = {
+    EmployeeId: 1,
+    FirstName: 'Vishu',
+    LastName: 'Gettam',
+    Dept: 'IT',
+    City: 'Chittoor',
+    Email: 'vishnu@gmail.com',
+  };
+  displayEmployee: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  EditEmp(empId: number) {
+    alert('Edit - ' + empId);
   }
 
+  UpdateEmp(empId: number) {
+    alert('Update - ' + empId);
+  }
 }
